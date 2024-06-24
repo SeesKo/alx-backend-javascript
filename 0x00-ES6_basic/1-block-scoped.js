7f9b7eebe59041085ef4f4ebce7d36f2b1288895
SeesKo <seeskosee@seeskome.com>
2024-06-24 05:05:26 -0700

    block scope

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,10 +1,10 @@
 export default function taskBlock(trueOrFalse) {
-  let task = false; // Use let instead of var
-  const task2 = true; // Use const instead of var
+  var task = false;
+  var task2 = true;
 
   if (trueOrFalse) {
-    task = true; // Update the existing variable
-    // task2 = false; // This will cause an error because const variables cannot be reassigned
+    const task = true;
+    const task2 = false;
   }
 
   return [task, task2];
