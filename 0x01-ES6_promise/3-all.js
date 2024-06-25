e251b8ed7cbc93fae6fe19f442947d36590e59c3
SeesKo <seeskosee@seeskome.com>
2024-06-24 22:56:21 -0700

    resolve all promises

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,13 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+  return Promise
+    .all([uploadPhoto(), createUser()])
+    .then((res) => {
+      const photoResponse = res[0];
+      const userResponse = res[1];
+
+      console.log(`${photoResponse.body} ${userResponse.firstName} ${userResponse.lastName}`);
+    })
+    .catch(() => console.log('Signup system offline'));
+}
