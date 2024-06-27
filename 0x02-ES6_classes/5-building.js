fdbd232d5a7d19e3c0ef708ad45097ac3ff33d84
SeesKo <seeskosee@seeskome.com>
2024-06-27 05:36:08 -0700

    implement building class

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,21 +1,20 @@
 export default class Building {
   constructor(sqft) {
-    this._sqft = sqft;
+    this.sqft = sqft;
+    if (this.constructor !== Building) {
+      if (typeof this.evacuationWarningMessage !== 'function') {
+        throw new Error(
+          'Class extending Building must override evacuationWarningMessage',
+        );
+      }
+    }
   }
 
-  // Getter for sqft
   get sqft() {
     return this._sqft;
   }
 
-  // Proxy method that calls the abstract method
-  callEvacuationWarningMessage() {
-    // Ensure `this` context is used
-    this.evacuationWarningMessageProxy();
-  }
-
-  // Abstract method: to be overridden by subclasses
-  evacuationWarningMessageProxy() {
-    throw new Error('Class extending Building must override evacuationWarningMessage');
+  set sqft(value) {
+    this._sqft = value;
   }
 }
