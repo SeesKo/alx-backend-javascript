2d6fcdfcd9aa1717077982a07764c11e258de55c
SeesKo <seeskosee@seeskome.com>
2024-06-27 11:15:54 -0700

    implement evCar class

---
+++ 0x02-ES6_classes/100-evcar.js
@@ -0,0 +1,20 @@
+import Car from './10-car';
+
+class EVCar extends Car {
+  constructor(brand, motor, color, range) {
+    super(brand, motor, color);
+    this._range = range;
+  }
+
+  // Getter for range
+  get range() {
+    return this._range;
+  }
+
+  // Override the cloneCar method to return an instance of Car
+  cloneCar() {
+    return new Car(this._brand, this._motor, this._color);
+  }
+}
+
+export default EVCar;
