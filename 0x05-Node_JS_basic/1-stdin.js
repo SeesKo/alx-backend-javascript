e966db59af913b2d3d261621854cb8af2a4fdce5
SeesKo <seeskome@seeskosee.com>
2024-08-19 17:25:11 +0000

    command line execution

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -6,7 +6,7 @@ process.stdout.write('Welcome to Holberton School, what is your name?\n');
 process.stdin.on('readable', () => {
   const input = process.stdin.read(); // Read from the input buffer
 
-  if (input) {
+  if (input !== null) {
     process.stdout.write(`Your name is: ${input}`);
   }
 });
