8abc0a0381f6abbaae0e22a9635e6cc19d08c410
SeesKo <seeskome@seeskosee.com>
2024-08-21 07:21:42 +0000

    sum function upgrade

---
+++ 0x06-unittests_in_js/2-calcul_chai.js
@@ -0,0 +1,19 @@
+// 2-calcul_chai.js
+
+const calculateNumber = (type, a, b) => {
+  const roundedA = Math.round(a);
+  const roundedB = Math.round(b);
+
+  switch (type) {
+    case 'SUM':
+      return roundedA + roundedB;
+    case 'SUBTRACT':
+      return roundedA - roundedB;
+    case 'DIVIDE':
+      return roundedB === 0 ? 'Error' : roundedA / roundedB;
+    default:
+      return 0;
+  }
+};
+
+module.exports = calculateNumber;
